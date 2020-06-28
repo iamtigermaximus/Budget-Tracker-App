ca04cec704dcb5e8405e78b294f95afa54570565
iamtigermaximus <46716968+iamtigermaximus@users.noreply.github.com>
2020-06-28 21:09:50 +0300

    Updates

---
+++ precache-manifest.a0d589675d326316966d6fb50167e904.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5dc5ebb6b3af59286fd270257a12340",
+    "url": "/SiegfredWebsite/index.html"
+  },
+  {
+    "revision": "42e54fc113c0c47e4377",
+    "url": "/SiegfredWebsite/static/css/main.c377cc84.chunk.css"
+  },
+  {
+    "revision": "baec8f5294b2d50149be",
+    "url": "/SiegfredWebsite/static/js/2.730939e5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SiegfredWebsite/static/js/2.730939e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e54fc113c0c47e4377",
+    "url": "/SiegfredWebsite/static/js/main.ff7d9835.chunk.js"
+  },
+  {
+    "revision": "42298b0f8b5725604454",
+    "url": "/SiegfredWebsite/static/js/runtime-main.6d81bde0.js"
+  },
+  {
+    "revision": "5a78036a4c5188f47d02f95fba191c36",
+    "url": "/SiegfredWebsite/static/media/BudgetApp.5a78036a.jpg"
+  },
+  {
+    "revision": "703849b645d62aa577fb3d2a06838ffa",
+    "url": "/SiegfredWebsite/static/media/CovidTrackerApp.703849b6.jpg"
+  },
+  {
+    "revision": "a1138b6e26e4d4b037d3108fa0531230",
+    "url": "/SiegfredWebsite/static/media/PortfolioParallax.a1138b6e.jpg"
+  },
+  {
+    "revision": "dc558c015e24f9d718ec2a938d61fee2",
+    "url": "/SiegfredWebsite/static/media/ResponsiveWebsite.dc558c01.jpg"
+  },
+  {
+    "revision": "e353315225a9fa88abe255caaf506df8",
+    "url": "/SiegfredWebsite/static/media/ToDoApp.e3533152.jpg"
+  },
+  {
+    "revision": "0c26685f11cac36831ad35c5489586eb",
+    "url": "/SiegfredWebsite/static/media/galaxy.0c26685f.jpg"
+  },
+  {
+    "revision": "2b0e776617aa0afd56af662633ddbe23",
+    "url": "/SiegfredWebsite/static/media/techfolio.2b0e7766.png"
+  }
+]);
